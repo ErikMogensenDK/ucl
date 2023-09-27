7fb9e75ff91d8a29def887ba6714f09950c54d31
ondonden <erikmogensen1@gmail.com>
2023-09-27 08:50:57 +0200

    removed testfolders

--- SemesterOne/newTestFolder/myConsoleApp/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace myConsoleApp;
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-    }
-}
--- SemesterOne/testfolder/testProject/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace testProject;
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-    }
-}
